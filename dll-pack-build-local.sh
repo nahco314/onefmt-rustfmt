aa8e2313d8e4f7ce81aba4434df01401c83e4108
nahco314 <nahco3_ta@yahoo.co.jp>
2026-03-21 10:50:19 +0900

    fix(release): resolve windows packaging for rustfmt 1.9

--- dll-pack-build-local.sh
+++ dll-pack-build-local.sh
@@ -6,16 +6,32 @@ if [ -z "${gh_tag}" ]; then
     gh_tag="${GITHUB_SHA:-local}"
 fi
 
+sysroot="$(rustc --print sysroot | sed 's|\\|/|g')"
+host_lib_dir="${sysroot}/lib/rustlib/$(rustc -vV | grep host | awk '{print $2}')/lib"
+target_dll="$(dll-pack-builder find "${BUILD_OUT_DIR}")"
+
+extra_dll_pack_args=(
+    --include "${host_lib_dir}/*"
+    --macho-rpath "${host_lib_dir}"
+    --win-path "${host_lib_dir}"
+)
+
+if [ -n "${SYSTEMROOT:-}" ]; then
+    system_root="$(printf '%s\n' "${SYSTEMROOT}" | sed 's|\\|/|g')"
+    extra_dll_pack_args+=(
+        --win-path "${system_root}/System32"
+        --win-path "${system_root}/SysWOW64"
+    )
+fi
+
 rustup target add "${DLL_PACK_TARGET}"
 
 cargo build --profile super-release --target "${DLL_PACK_TARGET}"
 
 mkdir -p ./artifacts/
 
-LD_LIBRARY_PATH=$(rustc --print sysroot)/lib/rustlib/$(rustc -vV | grep host | awk '{print $2}')/lib \
+LD_LIBRARY_PATH="${host_lib_dir}" \
     dll-pack-builder local $(cargo metadata --no-deps --format-version 1 | jq -r '.packages[0].name') \
-    $(dll-pack-builder find ${BUILD_OUT_DIR}) \
+    "${target_dll}" \
     ./artifacts/ "${DLL_PACK_TARGET}" "${GITHUB_REPOSITORY}" "${gh_tag}" \
-    --include "$(rustc --print sysroot | sed 's|\\|/|g')/lib/rustlib/$(rustc -vV | grep host | awk '{print $2}')/lib/*" \
-    --macho-rpath $(rustc --print sysroot)/lib/rustlib/$(rustc -vV | grep host | awk '{print $2}')/lib \
-    --win-path $(rustc --print sysroot)/lib/rustlib/$(rustc -vV | grep host | awk '{print $2}')/lib
+    "${extra_dll_pack_args[@]}"
