3de95e5772312386dda8a148b9cde91d9678aaa2
nahco314 <nahco3_ta@yahoo.co.jp>
2026-03-21 10:08:41 +0900

    fix(release): remove wasm release target

--- dll-pack-build-local.sh
+++ dll-pack-build-local.sh
@@ -1,13 +1,21 @@
-rustup target add ${DLL_PACK_TARGET}
+set -euo pipefail
 
-cargo build --profile super-release --target ${DLL_PACK_TARGET}
+gh_tag="${DLL_PACK_GH_TAG:-${GITHUB_REF_NAME:-${GITHUB_REF:-}}}"
+gh_tag="${gh_tag#refs/tags/}"
+if [ -z "${gh_tag}" ]; then
+    gh_tag="${GITHUB_SHA:-local}"
+fi
 
-mkdir ./artifacts/
+rustup target add "${DLL_PACK_TARGET}"
+
+cargo build --profile super-release --target "${DLL_PACK_TARGET}"
+
+mkdir -p ./artifacts/
 
 LD_LIBRARY_PATH=$(rustc --print sysroot)/lib/rustlib/$(rustc -vV | grep host | awk '{print $2}')/lib \
     dll-pack-builder local $(cargo metadata --no-deps --format-version 1 | jq -r '.packages[0].name') \
     $(dll-pack-builder find ${BUILD_OUT_DIR}) \
-    ./artifacts/ ${DLL_PACK_TARGET} ${GITHUB_REPOSITORY} ${GITHUB_REF#refs/tags/} \
+    ./artifacts/ "${DLL_PACK_TARGET}" "${GITHUB_REPOSITORY}" "${gh_tag}" \
     --include "$(rustc --print sysroot | sed 's|\\|/|g')/lib/rustlib/$(rustc -vV | grep host | awk '{print $2}')/lib/*" \
     --macho-rpath $(rustc --print sysroot)/lib/rustlib/$(rustc -vV | grep host | awk '{print $2}')/lib \
     --win-path $(rustc --print sysroot)/lib/rustlib/$(rustc -vV | grep host | awk '{print $2}')/lib
